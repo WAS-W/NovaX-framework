906eddf9f96950c2192874425cc39ba4a45b1240
WAS-W <sohyp611@gmail.com>
2026-09-10 22:29:41 +0300

    effect.js

--- src/effects/effect.js
+++ src/effects/effect.js
@@ -9,3 +9,33 @@ export function getElement(element_name, isList = false, code_list) {
         return document.querySelector(element_name);
     }
 }
+
+export function ForEach(ListName, callback) {
+    ListName.forEach((item, index) => callback(item, index))
+}
+
+export function useFetch(url, method, headers = {}, DataFunc, ErrFunc ) {
+    fetch(url,{
+        method: method,
+        headers: headers
+    })
+    .then(res => {
+        if (!res.ok) return throw Error(`HTTP Error! status: ${res.status}`)
+     })
+    .then(data => {
+        if (typeof DataFunc === "function") DataFunc(data)
+    })
+    .catch(err => {
+        if (typeof ErrFunc === "function") ErrFunc(err)
+    })
+}
+
+export function useCopy(text, onSuccess, onERR) {
+    window.navigator.clipboard.writeText(text)
+        .then(() =>{
+            if (typeof onSuccess === "function") onSuccess()
+        })
+        .catch(err => {
+            if (typeof onERR === 'function') onERR(err);
+        });
+}
